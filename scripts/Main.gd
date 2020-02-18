7d6daaa6bd25eb9f2dfb104c9d6a7940c2468262
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-02-18 01:32:54 +0100

    Boid now slows down on arrival (if seeking)

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,7 +4,7 @@ export(PackedScene) var boidScene = preload("res://scenes/Boid.tscn")
 
 func _ready() -> void:
 	var boid : Boid = boidScene.instance()
-	boid.setup($Start.position, $Target, Boid.BOID_MODE.SEEK)
+	boid.setup($Start.position)
 	self.add_child(boid)
 	
 func _input(event: InputEvent) -> void:
--- scripts/boidController.gd
+++ scripts/boidController.gd
@@ -6,6 +6,7 @@ enum BOID_MODE { SEEK, FLEE }
 
 export(float, 0.1, 12, 0.1) var MAX_SPEED = 5
 export(float, 0.05, 2, 0.05) var MAX_FORCE = 1
+export(float, 1, 200, 0.5) var ARRIVE_RADIUS = 20
 export(BOID_MODE) var DEFAULT_MODE = BOID_MODE.SEEK
 
 var velocity : Vector2 = Vector2(0,0)
@@ -21,22 +22,30 @@ func setup(pos : Vector2, t : Node = null, m = null):
 	self.mode = m if m != null else DEFAULT_MODE
 
 func _process(delta: float) -> void:
-	var tPos : Vector2 = getTargetPos()
-	look_at(tPos)
+	look_at(getTargetPos())
 	
-	self.acceleration = (getDesire() - self.velocity).clamped(MAX_FORCE)
-	self.velocity += self.acceleration
-	self.position += self.velocity
-	self.acceleration *= 0
+	var tDist : float = getTargetPos().distance_to(self.position)
+	
+	if (tDist >= 0.01):
+		self.acceleration = (getDesire() - self.velocity).clamped(MAX_FORCE)
+		self.velocity += self.acceleration
+		self.position += self.velocity
+		self.acceleration *= 0
 
 func getDesire() -> Vector2:
 	var tPos : Vector2 = getTargetPos()
-	var output = tPos - self.position
+	var tDist : float = tPos.distance_to(self.position)
+	var mult : float = MAX_SPEED
 	
+	var desire = tPos - self.position
 	if (self.mode == BOID_MODE.FLEE):
-		output *= -1
+		desire *= -1
+	elif (tDist <= ARRIVE_RADIUS):
+		mult = range_lerp(tDist, ARRIVE_RADIUS, 0, MAX_SPEED, 0)
+
+	desire = desire.normalized() * mult
 	
-	return output.normalized() * MAX_SPEED
+	return desire
 
 func getTargetPos() -> Vector2:
 	return get_global_mouse_position() if self.targetMouse else self.target.position
