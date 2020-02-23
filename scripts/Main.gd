82884c1cd4aba5a7c99447c46bf406fbcb0e5b2e
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-02-23 14:38:06 +0100

    Misc 2

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,9 +4,11 @@ export(PackedScene) var boidScene = preload("res://scenes/Boid.tscn")
 
 func _ready() -> void:
 	randomize()
-	var boid : Boid = boidScene.instance()
-	self.add_child(boid)
-	boid.setup($Start.position, $Target)
+
+	for i in range(0, 10):
+		var boid : Boid = boidScene.instance()
+		self.add_child(boid)
+		boid.setup($Start.position, $Target)
 	
 func _input(_event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(1):
